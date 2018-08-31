d7a08b45f0e8dd8a77e0da73e3d04ff32ee30a2e
Max Petersen <mglxp5@gmail.com>
2018-08-31 23:17:50 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,4 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(talk)
-  if talk != talk.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif talk == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif talk == talk.upcase
-    return "NO, NOT SINCE 1938"
 end
