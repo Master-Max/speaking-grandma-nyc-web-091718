4fd743622c1c2f3872570cf29c7132952a0c5453
Max Petersen <mglxp5@gmail.com>
2018-08-31 23:15:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,6 @@ def speak_to_grandma(talk)
     return "HUH?! SPEAK UP, SONNY!"
   elsif talk == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif talk 
+  elsif talk == talk.upcase
     return "NO, NOT SINCE 1938"
 end
